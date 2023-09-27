68f46fb9f70940fc91c5915fed5c9b53dce85734
FenriRagni <145153161+FenriRagni@users.noreply.github.com>
2023-09-26 20:51:35 -0400

    add comment with plan of attack

--- script.js
+++ script.js
@@ -28,5 +28,21 @@ function generatePassword(){
 
   }
 }
+
+//each characteristic exists in an array containing only those characters
+//generatePassword() should prompt for the length of the password
+//if length of password is > 128 or less < 8  give appropriate alert and exit generatePassword()
+//once passLength is within parameters:
+//prompt user to include Lowercase letters hold in var, if yes add lowerArray to includedArray, alert(confirming their choice)
+//prompt user to include Uppercase letters hold in var, if yes add upperArray to includedArray, alert(confirming their choice)
+//prompt user to include Numeric values hold in var, if yes add numArray to includedArray, alert(confirming their choice)
+//prompt user to include Special characters hold in var, if yes add specArray to includedArray, alert(confirming their choice)
+//if no characteristics are selected alert user and exit generatePassword()
+//for each chosen characteristic choose one at random from characteristic array to add to password
+//once one of each characteristic has been chosen we choose randomly from the includedArray until we reach the password length
+//we then output the password to the main screen
+
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
